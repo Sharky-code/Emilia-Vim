e0a355da69b088ba746e5576c8b300343dac962a
jack wong <jackwong2332@gmail.com>
2024-02-01 21:59:12 +0000

    changed lua mappings

--- lua/settings/mappings.lua
+++ lua/settings/mappings.lua
@@ -30,4 +30,3 @@ vim.keymap.set('i', '<c-h>', function () return vim.fn['codeium#Accept']() end, 
 vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
 vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
 vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-
